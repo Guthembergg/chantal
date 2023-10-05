d5ac8aa6aee239ba975630f341c65ca993aad6f2
Guthembergg <karimguettech@gmail.com>
2023-10-05 16:18:07 +0200

    added sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,6 +12,32 @@ Items
 Menu
 --------------------*/
 export default function Menu() {
+  const [toggle, setToggle] = useState(false);
+
+  const navLinks = [
+    {
+      id: "Home",
+      title: "Home",
+      href: "",
+    },
+    {
+      id: "Portfolio",
+      title: "Portfolio",
+      href: "Portfolio",
+    },
+    {
+      id: "Skills",
+      title: "Skills",
+      href: "Skills",
+    },
+
+    {
+      id: "Contacts",
+      title: "Contacts",
+      href: "Contacts",
+    },
+  ];
+
   const items = [
     {
       name: "About",
@@ -77,12 +103,12 @@ export default function Menu() {
   }, [active]);
 
   return (
-    <div ref={$root} className="menu  flex">
+    <div ref={$root} className="menu  ">
       {items.map((item, index) => (
         <a
           key={item.name}
           ref={$items.current[index]}
-          className={`item ${active === index ? "active" : ""}`}
+          className={`item  sm:flex hidden ${active === index ? "active" : ""}`}
           onMouseEnter={() => {
             setActive(index);
           }}
@@ -90,7 +116,71 @@ export default function Menu() {
         >
           {item.name}
         </a>
-      ))}
+      ))}{" "}
+      <div className="sm:hidden flex flex-1 justify-end toggle">
+        {toggle && (
+          <>
+            <span v-show="open" onClick={() => setToggle(!toggle)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </>
+        )}
+        {!toggle && (
+          <>
+            <span v-show="!open" onClick={() => setToggle(!toggle)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </span>
+          </>
+        )}
+
+        {/* Sidebar */}
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar bg-slate-500`}
+          style={{ zIndex: 1 }}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a className="" href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
       <div ref={$indicator1} className="indicator" />
       <div ref={$indicator2} className="indicator" />
     </div>
