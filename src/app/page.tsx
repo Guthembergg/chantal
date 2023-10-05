31753143cbb5a3354bfc47948eaf78bb9ac9f9f6
Guthembergg <karimguettech@gmail.com>
2023-10-05 22:23:17 +0200

    better css mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,8 +66,8 @@ export default function Home() {
       {/*  <Gallery enableImageSelection={true} images={images} /> */}
       {/*    <Toggle />{" "}*/}
       <div className="p-[10%] pt-10">
-        <div className="flex justify-center pb-10 mt-[10%]">
-          <h1 className=" text-4xl title">Chantal Stanzione</h1>
+        <div className="flex justify-center pb-10 mt-[100px]">
+          <h1 className="text-[25px] md:text-4xl title">Chantal Stanzione</h1>
         </div>{" "}
         <Toggle />
         <Masonry />
--- src/components/masonry/Masonry.jsx
+++ src/components/masonry/Masonry.jsx
@@ -15,73 +15,73 @@ export default class MyWrapper extends React.Component {
       <ResponsiveMasonry
         columnsCountBreakPoints={{ 350: 2, 450: 3, 900: 3, 1900: 3 }}
       >
-        <Masonry columnsCount={5} gutter="10px">
+        <Masonry gutter="10px">
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
-            <img src="/image1.jpeg" />{" "}
-          </LightGallery>{" "}
+            <img src="/image1.jpeg" />
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image2.jpeg" />
           </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image3.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image4.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image5.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image6.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image7.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image8.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image9.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image10.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image11.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image12.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image13.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image14.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image15.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image16.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image17.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image18.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image19.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image20.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image21.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image22.jpeg" />
-          </LightGallery>{" "}
+          </LightGallery>
           <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
             <img src="/image23.jpeg" />
           </LightGallery>
