f4d37391e7de87609a3f3b5f0aa58fad95574eac
Guthembergg <karimguettech@gmail.com>
2023-10-07 10:08:08 +0200

    footer fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,32 +9,9 @@ export default function App() {
       bgColor="light"
       className="text-center text-lg-start text-muted  bottom-0 footer "
     >
-      <section className="d-flex justify-content-center justify-content-lg-between p-4 sectionFooter">
-        <div className="me-5 d-none d-lg-block"></div>
+      <section className="d-flex justify-content-center justify-content-lg-between  sectionFooter"></section>
 
-        <div>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="facebook-f" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="twitter" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="google" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="instagram" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="linkedin" />
-          </a>
-          <a href="" className="me-4 text-reset">
-            <i color="secondary" fab className="github" />
-          </a>
-        </div>
-      </section>
-
-      <div className="text-center pt-4 pb-3">
+      <div className="text-center pt-3 pb-3">
         © 2023 {"  "}
         <a className="text-reset " href="https://mdbootstrap.com/">
           Chantal stanzione
