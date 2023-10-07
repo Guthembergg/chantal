ef1ec10a92ee97c930bf36149bde7556bde452bf
Guthembergg <karimguettech@gmail.com>
2023-10-07 10:28:23 +0200

    title css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,14 +5,14 @@ import "./style.css";
 export default function About() {
   return (
     <main className="h-100 flex justify-center content-center">
-      <div className="p-[30px]   lg:p-[100px] flex justify-center  ">
-        <div className="flex justify-center pb-10 	about	">
+      <div className="p-[30px]  pt-[150px] md:pt-[50x] lg:p-[100px] flex justify-center content-center ">
+        <div className="flex justify-center pb-10 about	">
           <div className="person flex mr-7 translateLeft">
             <img src="/person.jpg" className=" m-auto" />
           </div>
 
-          <div className="flex flex-col justify-center  w-100 translateRight ps-10">
-            <h1 className="text-[25px] text-3xl text-left title font-thin mt-10  md:pb-[10px] md:mt-0 ">
+          <div className="flex flex-col justify-center  w-100 translateRight ">
+            <h1 className="text-[25px] text-3xl  title  text-center md:text-left font-thin mt-10  md:pb-[10px] md:mt-0 ">
               Chantal Stanzione
             </h1>
             <p className="mt-10  title text-xl  text-left">
