6026ce863f9c12fedb763e047c9e4e7fd52d1c3f
Guthembergg <karimguettech@gmail.com>
2023-10-07 14:44:49 +0200

    -

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -88,7 +88,7 @@ const ContactForm = () => {
           </div>
           <div className="pt-0 mb-3">
             <button
-              className="active:bg-slate-600 hover:shadow-lg focus:outline-none px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-slate-400 rounded shadow outline-none"
+              className="active:bg-gray-500 hover:shadow-lg focus:outline-none px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-gray-400 rounded shadow outline-none"
               type="submit"
             >
               Send a message
